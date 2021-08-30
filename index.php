adabda3f8bfe44b609e32443b8014dd8688f4cc2
Viviam <vivian.lopesg@gmail.com>
2021-08-29 23:33:57 -0300

    criando a interface

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<?php include("layout/head.php");?>
+
+<body>
+
+  <?php include("layout/sidebar.php");?>
+  <!-- Conteudo da Página -->
+  <header>
+    <h1>Books Kingdom</h1>
+  </header>
+  <div class="main">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <canvas id="myChart"></canvas>
+        </div>
+        <div class="col-md-6">
+          <canvas id="myChart2"></canvas>
+        </div>     
+     </div>
+    </div>
+    <footer>
+      <a href="sobre.html">Sobre nós</a>
+    </footer>
+  </div>
+
+</body>
+
+</html>
---
+++ layout/head.php
@@ -0,0 +1,32 @@
+<head>
+    <!--css-->
+    <link rel="stylesheet" href="plugin/css/font-awesome.css">
+    <link rel="stylesheet" href="plugin/css/bootstrap.css">
+    <link rel="stylesheet" href="plugin/css/Chart.min.css">
+    <link rel="stylesheet" href="plugin/css/solid.css">
+    <link rel="stylesheet" href="plugin/css/jquery-confirm.css">
+    <link rel="stylesheet" href="plugin/css/datatables.css">
+    <!--my css-->
+    <link rel="stylesheet" href="plugin/css/style.css">
+
+   <!--js-->
+   <script src="plugin/js/jquery-3.5.1.js" type="text/javascript"></script>
+   <script src="plugin/js/bootstrap.js"></script>
+   <script src="plugin/js/jquery-confirm.js"></script>
+   <script src="plugin/js/Chart.min.js"></script>
+   <script src="plugin/js/datatables.js"></script>
+   <script src="plugin/js/dataTables.buttons.min.js"></script>
+   <script src="plugin/js/buttons.flash.min.js"></script>
+  <script src="plugin/js/jszip.min.js"></script>
+  <script src="plugin/js/pdfmake.min.js"></script>
+  <script src="plugin/js/vfs_fonts.js"></script>
+  <script src="plugin/js/buttons.html5.min.js"></script>
+  <script src="plugin/js/buttons.print.min.js"></script>
+
+
+   <!--my js-->
+   <script src="plugin/js/mycharts.js"></script>
+   <script src="plugin/js/mytables.js"></script>
+   <script src="plugin/js/functions.js"></script>
+   
+</head>
