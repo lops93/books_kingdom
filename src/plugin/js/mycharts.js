a759c93a8fbcc92049d04951f811b2f0684dbc4b
Viviam <vivian.lopesg@gmail.com>
2021-09-16 00:15:44 -0300

    terceiro grafico

--- src/plugin/js/mycharts.js
+++ src/plugin/js/mycharts.js
@@ -105,4 +105,74 @@ $(document).ready(function() {
         }
       });  
 
+
+      var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May"];
+    
+      var ctx = document.getElementById("myChart3");
+       ctx.height = 300;  
+      var myChart3 = new Chart(ctx, {
+          type: 'line',
+          data: {
+              labels: monthNames,
+              datasets: [{
+                  label: "Net sales",
+                  data: [20,30,40,50,60],
+                  pointHoverBackgroundColor:  'rgb(255, 99, 132)',  
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)'
+                  ],
+                  borderColor: [
+                      'rgba(255,99,132,1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 159, 64, 1)'
+                  ],
+                  borderWidth: 1
+              },
+              {
+                  label: "Gross sales",
+                  data: [20,60,40,80,70],
+                  pointHoverBackgroundColor:  'rgb(66, 133, 244)',   
+                  backgroundColor: [
+                      'rgb(66, 133, 244, 0.2)',                 
+                  ],
+                  borderColor: [
+                      'rgb(66, 133, 244)',                  
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+            maintainAspectRatio: false,
+            title:{
+              display: "true",
+              text: "Sales ",
+              fontSize: "18"
+            },
+            tooltips:{
+              intersect:false,
+              mode: 'index'
+            },
+            hover: {
+              mode: 'index',
+              intersect: false
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+          }
+      });
+      
+    
 });
